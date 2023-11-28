8db40dd6548e593345cb7202c90d1b5ae6271a8c
shopa.tomek@gmail.com <shopa.tomek@gmail.com>
2023-11-28 16:39:03 +0100

    database view updated, brake added line 20

--- resources/views/database.blade.php
+++ resources/views/database.blade.php
@@ -6,10 +6,8 @@
     <div class="row justify-content-center">
 
         <div class="col-md-8">
-
             <div class="card">
                 <div class="card-header">{{ __('Database') }}</div>
-
                 <div class="card-body">
                     @if (session('status'))
                     <div class="alert alert-success" role="alert">
@@ -17,19 +15,37 @@
                     </div>
                     @endif
 
-                    {{ __('Database') }}
+                    {{ __('Here will be some data') }}
                     <br>
-                    <div class="col-md-3">
-                        <div>
+                    <br>
 
+                    <div class="row justify-content-center"> <!-- Utworzenie wiersza, który będzie wyśrodkowany -->
+                        <div class="col-md-6"> <!-- Utworzenie kolumny dla Users -->
+                            <div class="card">
+                                <div class="card-body">
+                                    Users Count:
+                                </div>
+                            </div>
                         </div>
-                        <div>
-
+                        <div class="col-md-3"> <!-- Utworzenie kolumny dla Name -->
+                            <div class="card">
+                                <div class="card-body">
+                                    Name
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3"> <!-- Utworzenie kolumny dla Email -->
+                            <div class="card">
+                                <div class="card-body">
+                                    Email
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+
     </div>
 </div>
 @endsection
