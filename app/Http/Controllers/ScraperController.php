3038f0a73d72fb9fd2e69d1a1c7937995067a442
shopa.tomek@gmail.com <shopa.tomek@gmail.com>
2023-12-07 17:30:46 +0100

    ScrapperController method updated. Requested data displays correclty

--- app/Http/Controllers/ScraperController.php
+++ app/Http/Controllers/ScraperController.php
@@ -12,12 +12,39 @@ class ScraperController extends Controller
     public function scrap()
     {
         $client = new Client();
-        // $url = 'https://www.tiktok.com/@theaitoolreport';
-        $url = 'https://meteo.pl/';
+        $url = 'https://www.tiktok.com/@theaitoolreport';
+        // $url = 'https://meteo.pl/';
         $page = $client->request('GET', $url);
-        echo $page->filter(selector: '#kon_4c')->text();
+        // echo $page->filter(selector: '.tiktok-12ijsdd-H3CountInfos')->text();
 
+        // Używamy selektorów CSS do znalezienia odpowiednich danych
+        $followingSelector = 'strong[title="Following"]';
+        $followersSelector = 'strong[title="Followers"]';
+        $likesSelector = 'strong[title="Likes"]';
 
+        // Sprawdzamy, czy elementy istnieją na stronie
+        if ($page->filter($followingSelector)->count() > 0) {
+            $following = $page->filter($followingSelector)->text();
+        } else {
+            $following = 'Not found';
+        }
+
+        if ($page->filter($followersSelector)->count() > 0) {
+            $followers = $page->filter($followersSelector)->text();
+        } else {
+            $followers = 'Not found';
+        }
+
+        if ($page->filter($likesSelector)->count() > 0) {
+            $likes = $page->filter($likesSelector)->text();
+        } else {
+            $likes = 'Not found';
+        }
+
+        // Wyświetlanie wyników
+        echo "Following: $following\n";
+        echo "Followers: $followers\n";
+        echo "Likes: $likes\n";
         // echo "<pre>";
         // print_r($page);
     }
