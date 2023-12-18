57076fe0cda78b38bb95637e5b8b5d56f52fa5bb
shopa.tomek@gmail.com <shopa.tomek@gmail.com>
2023-12-18 21:21:43 +0100

    images added

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,21 +1,35 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container">
-        <h1>{{ $heading }}</h1>
+    @foreach ($listings as $listing)
+        <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+            <div class="container">
+                <div class="flex">
+                    <img class="hidden w-48 mr-6 md:block" src="images/acme.png" alt="" />
+                    <div>
+                        <div class="text-lg mt-4">
+                            <i class="fa-solid fa-location-dot"></i> {{ $listing->location }}
+                        </div>
+                    </div>
+                </div>
+                {{-- <h1>{{ $heading }}</h1> --}}
 
-        @if (count($listings) == 0)
-            <p>No listing found</p>
-        @endif
-
-        @foreach ($listings as $listing)
-            <h2>
-                <a href="/listings/{{ $listing['id'] }}">
-                    {{ $listing['title'] }}
-                </a>
-            </h2>
+                @if (count($listings) == 0)
+                    <p>No listing found</p>
+                @endif
 
 
-            <p>{{ $listing['description'] }}</p>
-        @endforeach
-    </div>
+                <h2>
+                    <a href="/listings/{{ $listing['id'] }}">
+                        {{ $listing['title'] }}
+                    </a>
+                </h2>
+
+
+                <p>{{ $listing['description'] }}</p>
+
+            </div>
+        </div>
+    @endforeach
 @endsection
+{{-- https://www.youtube.com/watch?v=MYyJ4PuL4pY&ab_channel=TraversyMedia --}}
+{{-- 1:15 --}}
