248e3229930e3d58a2d4a4479c2e1a3d6cbc1715
shopa.tomek@gmail.com <shopa.tomek@gmail.com>
2023-12-27 17:57:44 +0100

    scopefilter function filtering the the search bar added to Listi ng model and views

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -11,7 +11,7 @@ class ListingController extends Controller
     {
 
         return view('listings', [
-            'listings' => Listing::latest()->filter(request(['tag']))->get()
+            'listings' => Listing::latest()->filter(request(['tag', 'search']))->get()
         ]);
     }
 }
--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,22 +1,8 @@
 @extends('layouts.app')
 @section('content')
     <div class="container">
-        <form action="">
-            <div class="relative border-2 border-gray-100 m-4 rounded-lg">
-                <div class="absolute top-4 left-3">
-                    <i class="fa fa-search text-gray-400 z-20 hover:text-gray-500"></i>
-                </div>
-                <input type="text" name="search"
-                    class="h-14 w-full pl-10 pr-20 rounded-lg z-0 focus:shadow focus:outline-none"
-                    placeholder="Search job..." />
-                <div class="absolute top-2 right-2">
-                    <button type="submit" class="h-10 w-20 text-white rounded-lg bg-red-500 hover:bg-red-600">
-                        Search
-                    </button>
-                </div>
-            </div>
-        </form>
-        <a href="/listings/" class="inline-block text-black ml-4 mb-4"><i class="fa-solid fa-arrow-left"></i> Back
+        @include('partials._search')
+        <a href="/listings" class="inline-block text-black ml-4 mb-4"><i class="fa-solid fa-arrow-left"></i> Back
         </a>
         <div class="mx-4">
             <div class="bg-gray-50 border border-gray-200 p-10 rounded">
